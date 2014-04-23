739b7986eeefa4bc0942442a5b675df8b945f9cc
rayd25051992 <rayd_250592@outlook.com>
2014-04-23 22:42:28 +0100

    UI designed

---
+++ TVQuiz/TVQuiz/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using TVQuiz.Resources;
+
+namespace TVQuiz
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
